12fc3f49dcd2b05991bf1b73223eff6d2a04fac6
JcDecour <doudouc27@gmail.com>
2016-12-15 17:30:11 +0100

    en cours

---
+++ app/Views/front/customer_profil.php
@@ -0,0 +1,165 @@
+<?php $this->layout('layout', ['title' => 'Profil']) ?>
+<?php $this->start('main_content') ?>
+<div class="page-header">
+  <h1 style="text-align: center;">Mon profil</h1>
+</div>
+<div class="forms">
+  <form method="post" class="form-horizontal">
+    <p class="text-required-filed">
+    </p>
+    <fieldset>
+      <?php if(isset($formErrors['global'])): ?>
+      <div class="alert alert-danger">
+        <?=$formErrors['global'];?>
+      </div>
+      <?php endif; ?>
+
+      <!-- Select civilité -->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="civilité">Civilité :
+        </label>
+        <div class="col-md-2">
+          <select id="civilité" name="civilité" class="form-control" required="">
+            <option value="" selected disabled>- sélection -</option>
+            <option value="Monsieur">Mr</option>
+            <option value="Madame">Mme</option>
+            <option value="Mademoiselle">Melle</option>
+          </select>
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['civilité'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['civilité']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Firstname-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="firstname">Prénom :
+        </label>
+        <div class="col-md-4">
+          <input id="firstname" name="firstname" type="text" value="<?=isset($post['firstname']) ? $post['firstname'] : '';?>"  class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['firstname'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['firstname']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Lastname-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="lastname">Nom :
+        </label>
+        <div class="col-md-4">
+          <input id="lastname" name="lastname" type="text" class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['lastname'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['lastname']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="email">Email:
+          <span class="obligatoire">*</span>
+        </label>
+        <div class="col-md-4">
+          <input id="email" name="email" type="email" class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['email'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['email']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Password input-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="password">Mot de passe:
+        </label>
+        <div class="col-md-4">
+          <input id="password" name="password" type="password" class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['password'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['password']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="fixed_phone">Téléphone fixe:
+        </label>
+        <div class="col-md-4">
+          <input id="fixed_phone" name="fixed_phone" type="text" class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['fixed_phone'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['fixed_phone']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="mobile_phone">Téléphone mobile:
+        </label>
+        <div class="col-md-4">
+          <input id="mobile_phone" name="mobile_phone" type="text"  class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['mobile_phone'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['mobile_phone']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="street">Adresse:
+          <span class="obligatoire">*</span>
+        </label>
+        <div class="col-md-4">
+          <input id="street" name="street" type="text" placeholder="Rue de la paix" class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['street'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['street']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="zipcode">Code postal:
+          <span class="obligatoire">*</span>
+        </label>
+        <div class="col-md-2">
+          <input id="zipcode" name="zipcode" type="text" placeholder="75000" class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['zipcode'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['zipcode']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Text input-->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="city">Ville:
+          <span class="obligatoire">*</span>
+        </label>
+        <div class="col-md-4">
+          <input id="city" name="city" type="text" placeholder="Paris" class="form-control input-md">
+          
+        </div>
+        <!-- Gestion des erreurs -->
+        <?php if(isset($formErrors['city'])): ?>
+        <div class="error col-md-offset-4 col-md-8"><?=$formErrors['city']?></div>
+        <?php endif; ?>
+      </div>
+      <!-- Button -->
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="signin"></label>
+        <div class="col-md-4">
+          <button id="signin" name="signin" class="btn btn-info">Inscription</button>
+        </div>
+      </div>
+    </fieldset>
+  </form>
+</div>
+<?php $this->stop('main_content') ?>
