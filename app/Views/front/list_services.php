3c417ffe53c880a45eb0d812725d3405b4fc4d3d
lilic31 <alibert.eric@gmail.com>
2016-12-17 21:43:52 +0100

    Quelques ajustements de mise en forme

--- app/Views/front/list_services.php
+++ app/Views/front/list_services.php
@@ -25,6 +25,7 @@
 				<th>Service</th>
 				<th>Créé le</th>
 				<th>Prévu le</th>
+                <th class="text-center">Statut</th>
 				<th class="text-center">Devis</th>
 				<th class="text-center">Action</th>
 			</tr>
@@ -40,28 +41,40 @@
 					<?php if($project['closed']): ?>
 						<span class="text-danger">Cloturé</span>
 					<?php else: ?>
-						<span class="badge badge-success"><?=$project['nb_devis'];?></span>
+						<span class="text-success">Ouvert</span>
 					<?php endif; ?>	
 				</td>
+                <td class="text-center"><span class="badge badge-success"><?=$project['nb_devis'];?></span></td>
 				<td class="text-center">
-				<?php if(!$project['closed']): ?>
-					<a href="<?=$this->url('front_delete_service', ['id' => $project['id']]);?>" class="btn btn-danger btn-sm" title="Supprimer ce service">
-					 Supprimer
-					</a>
-					&nbsp; 
-					<a href="<?=$this->url('front_edit_service', ['idProject' => $project['id']]);?>" class="btn btn-info btn-sm" title="Modifier ce service">
-					 Modifier
-					</a>
-					&nbsp; 
-                    <a href="<?=$this->url('front_view_service', ['id' => $project['id']]);?>" class="btn btn-success btn-sm" title="Consulter ce service">
-					 Consulter
-					</a>
-                <?php else: ?>
-                    <a href="<?=$this->url('front_devis_view_customer', ['id' => $project['id']]);?>" class="btn btn-success btn-sm" title="Consulter ce service">
-					 Consulter
-					</a>
-				<?php endif; ?>
-					
+                    <?php if(!$project['closed']): ?>
+                        <div class="col-md-3">
+                            <a href="<?=$this->url('front_view_service', ['id' => $project['id']]);?>" class="btn btn-success btn-sm btn_margin" title="Consulter ce service">
+                             Consulter
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="<?=$this->url('front_edit_service', ['idProject' => $project['id']]);?>" class="btn btn-info btn-sm btn_margin" title="Modifier ce service">
+                             Modifier
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="<?=$this->url('front_delete_service', ['id' => $project['id']]);?>" class="btn btn-danger btn-sm btn_margin" title="Supprimer ce service">
+                             Supprimer
+                            </a>
+                        </div>
+                    <?php else: ?>
+                        <div class="col-md-3">
+                            <a href="<?=$this->url('front_devis_view_customer', ['id' => $project['id']]);?>" class="btn btn-success btn-sm btn_margin" title="Consulter ce service">
+                             Consulter
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            &nbsp;
+                        </div>
+                        <div class="col-md-3">
+                            &nbsp;
+                        </div>
+                    <?php endif; ?>
 				</td>
 			</tr>
 			<?php endforeach; ?>
