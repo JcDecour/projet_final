838765d4f4107e50027301628fc858907b7bb3a7
JcDecour <doudouc27@gmail.com>
2016-12-19 23:06:30 +0100

    fix navbar en responsive

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -27,7 +27,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="<?= $this->url('front_default_index') ?>">DevisRama</a>
+                <a class="navbar-brand" href="<?= $this->url('front_default_index') ?>">DEVIRAMA</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="navbar-collapse-1">
